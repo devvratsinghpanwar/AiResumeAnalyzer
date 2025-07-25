bed8c96eeb9bab3b7bfcad422520ba497e884510
marcus choudhary <devvratsingh0121@gmail.com>
2025-07-25 10:26:29 +0530

    second commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "FitFinder" },
+    { name: "description", content: "Welcome to resume analyzer powered by ai" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main>
+      hello
+    </main>
+  )
 }
